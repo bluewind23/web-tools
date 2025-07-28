59237c1f0901eeb04eab2f5f5cb7ae127afaa6d4
bluewind23 <jin5410@naver.com>
2025-07-29 08:16:24 +0900

    netlify_issue_update

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // output: 'export',  // 정적 익스포트로 설정
+  output: 'export',  // 정적 익스포트로 설정
   trailingSlash: true,  // URL 끝에 슬래시 추가
   images: {
     unoptimized: true,  // 정적 익스포트에서는 이미지 최적화 비활성화
